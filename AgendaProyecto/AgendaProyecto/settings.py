0183eead63a4855d57dd0c1c614b149c1183d4b3
vBlackBearr <Luispuentes.pgo@outlook.com>
2023-12-15 12:49:31 -0600

    sdsd

--- AgendaProyecto/AgendaProyecto/settings.py
+++ AgendaProyecto/AgendaProyecto/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'AgendaProyecto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'equipo1',
+        'USER': 'root',
+        'PASSWORD': '12345678',
+        'HOST': 'database-itec.crvlwakzi7le.us-east-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
